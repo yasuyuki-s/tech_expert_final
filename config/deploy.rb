17978c074f62dfc0419fbe311dbcbc68ee80d991
菅原康之 <yasu@yasu-Mac.local>
2017-11-04 19:02:48 +0900

    自動デプロイの対象ブランチを指定
    Masterブランチのみを自動デプロイの対象とする

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.10.0"
 
 set :application, 'tech_expert_final'
 set :repo_url,  'git@github.com:yasuyuki-s/tech_expert_final.git'
+set :branch,  'master'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
