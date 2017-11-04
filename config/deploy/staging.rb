7614333256f652b18ec50ee485d69737fde1c683
菅原康之 <yasu@yasu-Mac.local>
2017-11-04 18:44:49 +0900

    config設定の修正
    config/deploy.rbとconfig/deploy/staging.rbの設定に誤りがあったため修正

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,24 +1,2 @@
-# config valid only for current version of Capistrano
-lock '3.10.0'
+server '52.199.66.168', user: 'ec2-user', roles: %w{app db web}
 
-set :application, 'tech_expert_final'
-set :repo_url,  'git@github.com:yasuyuki-s/tech_expert_final.git'
-
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
-
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/yasu/.ssh/ec2_user.pem']
-
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
