929c1c102433da1d08e78f5c00fcdcb17d4dcc7d
菅原康之 <yasu@yasu-Mac.local>
2018-01-06 14:08:39 +0900

    ImageUploaderクラスを編集
    ・保存時の画像サイズを最大200x200に指定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -21,6 +21,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+
+  process resize_to_fill: [200, 200, "Center"]
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
