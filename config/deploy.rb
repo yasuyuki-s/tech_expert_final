d661af382f4a187ee05fa6fa166688663aa88559
菅原康之 <yasu@yasu-Mac.local>
2018-01-07 23:46:55 +0900

    デプロイ設定の編集
    secrets.ymlへのリンクを設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url,  'git@github.com:yasuyuki-s/tech_expert_final.git'
 set :branch,  'master'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
+set :linked_files, %w{ config/secrets.yml }
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 set :bundle_path, -> { shared_path.join('vendor/bundle') }
