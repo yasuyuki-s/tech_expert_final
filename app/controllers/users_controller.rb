a56c060049abff580112039303c0201f30e46422
菅原康之 <yasu@yasu-Mac.local>
2018-01-08 14:07:51 +0900

    ルーティングを変更
    ・tweetsコントローラー用にindexとcreateを設定
    ・rootをtweets#indexに変更
    ・ビューを再配置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show,:update]
 
-  def index
-
-  end
-
   def show
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'tweets#index'
 
   resources :users, only: [:index,:show,:update] , param: :name do
     collection do
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
       get :check_name
     end
   end
+
+  resources :tweets, only: [:index,:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
