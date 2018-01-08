559bc3d5a5edf37d87adbecd2a54430cb1324e2e
菅原康之 <yasu@yasu-Mac.local>
2018-01-08 11:14:37 +0900

    マイページのjsを設定
    ・ヘッダー画像編集ボタンの挙動を定義
    ・挙動はアバター画像編集ボタンに準じる

--- app/assets/javascripts/profileediting.js
+++ app/assets/javascripts/profileediting.js
@@ -2,6 +2,7 @@ $(document).on('turbolinks:load',function(){
 
   function toggle_ProfileEdit(){
     $(".ProfilePage-editingButtons").toggle();
+    $(".ProfileHeaderEditing").toggle();
     $(".ProfileHeaderCard").toggle();
     $(".ProfileHeaderCardEditing").toggle();
     $(".ProfilePage-editingOverlay").toggle();
@@ -25,12 +26,26 @@ $(document).on('turbolinks:load',function(){
     $("#avatar-dropdown-menu").toggle();
   });
 
+  $(".ProfileHeaderEditing-button").on("click",function(e){
+    e.preventDefault();
+    $("#header-dropdown-menu").toggle();
+  });
+
   $("#avatar-choose-button").on("click",function(e){
     e.preventDefault();
     $("#user_image_avatar").click();
   });
 
+  $("#header-choose-button").on("click",function(e){
+    e.preventDefault();
+    $("#user_image_canopy").click();
+  });
+
   $("#user_image_avatar").change(function(){
+    $(".edit_user").submit();
+  });
+
+  $("#user_image_canopy").change(function(){
     $(".edit_user").submit();
   });
 
@@ -40,12 +55,19 @@ $(document).on('turbolinks:load',function(){
     $(".edit_user").submit();
   });
 
+  $("#header-drop-button").on("click",function(e){
+    e.preventDefault();
+    $("#user_remove_image_canopy").prop('checked', true);
+    $(".edit_user").submit();
+  });
+
   $("#avatar-cancel-button").on("click",function(e){
     e.preventDefault();
     $("#avatar-dropdown-menu").toggle();
   });
 
-
-
-
+  $("#header-cancel-button").on("click",function(e){
+    e.preventDefault();
+    $("#header-dropdown-menu").toggle();
+  });
 });
