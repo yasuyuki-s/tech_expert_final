5f18d489ce08681f5a9dddf1e3fba40c3fdc6c5c
菅原康之 <yasu@yasu-Mac.local>
2018-01-08 19:37:37 +0900

    トップバーからのツイート機能を実装
    ・トップバーのツイートボタンクリック時のビューを部分テンプレートとして切り出し
    ・topbar.jsにトップバーのツイートボタンクリック時の挙動を追加

--- app/assets/javascripts/topbar.js
+++ app/assets/javascripts/topbar.js
@@ -1,9 +1,23 @@
 $(document).on('turbolinks:load',function(){
   over_flg = false;
+  over_flg2 = false;
+
+  $("#tweet-topbar-button").on("click",function(e){
+    e.preventDefault();
+    $(".TweetStormDialog").toggle();
+  });
+
   $("#topbar-dropdown-toggle").on("click",function(e){
     e.preventDefault();
     $("#topbar-dropdown-menu").toggle();
   });
+
+  $(".modal-content,#tweet-topbar-button").hover(function(){
+    over_flg2 = true;
+  }, function(){
+    over_flg2 = false;
+  });
+
   $("#topbar-dropdown-menu,#topbar-dropdown-toggle").hover(function(){
     over_flg = true;
   }, function(){
@@ -12,6 +26,9 @@ $(document).on('turbolinks:load',function(){
   $("body").on("click",function(){
     if(over_flg == false){
       $("#topbar-dropdown-menu").hide();
+    }
+    if(over_flg2 == false){
+      $(".TweetStormDialog").hide();
     }
   });
 });
