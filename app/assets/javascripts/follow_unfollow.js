25f26f70736b45808e4aee48838a4d203005a1c3
菅原康之 <yasu@yasu-Mac.local>
2018-01-15 00:38:35 +0900

    非同期通信によるフォロー・アンフォロー時のJSの挙動を修正
    どのページで押されたかによって、表示中のフォローおよびフォロワー数の増減を設定する

--- app/assets/javascripts/follow_unfollow.js
+++ app/assets/javascripts/follow_unfollow.js
@@ -3,6 +3,8 @@ $(document).on('turbolinks:load',function(){
   $(".follow-button > .EdgeButton").on("click", function(e){
     e.preventDefault();
     parent_wrapper = $(this).closest(".follow-button-wrapper");
+    third_party = $(this).hasClass("third-party");
+    counterpart = $(this).hasClass("counterpart");
     $.ajax({
       type: "POST",
       url: "/follow_relationships/",
@@ -10,8 +12,13 @@ $(document).on('turbolinks:load',function(){
       dataType: "json"
     })
     .done(function(data){
-      $("#follows_count").text(Number($("#follows_count").text())+ 1);
-      $("#followers_count").text(Number($("#followers_count").text())+ 1);
+      if (!third_party){
+        if (!counterpart){
+          $("#follows_count").text(Number($("#follows_count").text())+ 1);
+        }else{
+          $("#followers_count").text(Number($("#followers_count").text())+ 1);
+        };
+      };
       parent_wrapper.toggleClass("following-button-wrapper");
       parent_wrapper.toggleClass("follow-button-wrapper");
     })
@@ -23,6 +30,8 @@ $(document).on('turbolinks:load',function(){
   $(".following-button > .EdgeButton").on("click", function(e){
     e.preventDefault();
     parent_wrapper = $(this).closest(".following-button-wrapper");
+    third_party = $(this).hasClass("third-party");
+    counterpart = $(this).hasClass("counterpart");
     $.ajax({
       type: "DELETE",
       url: "/follow_relationships/",
@@ -30,8 +39,13 @@ $(document).on('turbolinks:load',function(){
       dataType: "json"
     })
     .done(function(data){
-      $("#follows_count").text(Number($("#follows_count").text())- 1);
-      $("#followers_count").text(Number($("#followers_count").text())- 1);
+      if (!third_party){
+        if (!counterpart){
+          $("#follows_count").text(Number($("#follows_count").text())- 1);
+        }else{
+          $("#followers_count").text(Number($("#followers_count").text())- 1);
+        };
+      };
       parent_wrapper.toggleClass("follow-button-wrapper");
       parent_wrapper.toggleClass("following-button-wrapper");
     })
