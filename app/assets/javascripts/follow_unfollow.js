11c299252d1cc2dfddd91b80ce1b9d99e40de599
菅原康之 <yasu@yasu-Mac.local>
2018-01-14 18:09:29 +0900

    フォロー、アンフォローボタンをAjax化
    通常のフォーム処理を上書きして、Ajaxによる非同期通信を実装

---
+++ app/assets/javascripts/follow_unfollow.js
@@ -0,0 +1,39 @@
+$(document).on('turbolinks:load',function(){
+
+  $(".follow-button > .EdgeButton").on("click", function(e){
+    e.preventDefault();
+    parent_wrapper = $(this).closest(".follow-button-wrapper");
+    $.ajax({
+      type: "POST",
+      url: "/follow_relationships/",
+      data: { follow_to_id: $(this).prev().attr("value") },
+      dataType: "json"
+    })
+    .done(function(data){
+      parent_wrapper.toggleClass("following-button-wrapper");
+      parent_wrapper.toggleClass("follow-button-wrapper");
+    })
+    .fail(function(){
+      alert("フォローに失敗しました");
+    });
+  });
+
+  $(".following-button > .EdgeButton").on("click", function(e){
+    e.preventDefault();
+    parent_wrapper = $(this).closest(".following-button-wrapper");
+    $.ajax({
+      type: "DELETE",
+      url: "/follow_relationships/",
+      data: { follow_to_id: $(this).prev().attr("value") },
+      dataType: "json"
+    })
+    .done(function(data){
+      parent_wrapper.toggleClass("follow-button-wrapper");
+      parent_wrapper.toggleClass("following-button-wrapper");
+    })
+    .fail(function(){
+      alert("アンフォローに失敗しました");
+    });
+  });
+
+});
--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -2,16 +2,22 @@ class FollowRelationshipsController < ApplicationController
 
   def create
     user = User.find(params[:follow_to_id])
-    current_user.follow!(user)
-
-    redirect_to root_path
+    if current_user.follow!(user)
+      respond_to do |format|
+        format.html {redirect_to root_path}
+        format.json
+      end
+    end
   end
 
   def destroy
     user = User.find(params[:follow_to_id])
-    current_user.unfollow!(user)
-
-    redirect_to root_path
+    if current_user.unfollow!(user)
+      respond_to do |format|
+        format.html {redirect_to root_path}
+        format.json
+      end
+    end
   end
 
 end
