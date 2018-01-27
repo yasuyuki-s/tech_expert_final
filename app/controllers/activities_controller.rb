ffb8f3734335c11394b72731ba4884db20dd0bfd
菅原康之 <yasu@yasu-Mac.local>
2018-01-27 20:17:42 +0900

    ユーザーのここ一ヶ月間のツイート数をグラフ表示
    ・ユーザー自身
    ・ユーザーのフォロー先
    ・ユーザーのフォロワー
    以上3つの対象について、ここ一ヶ月間のツイート数をアクティビティとして表示する

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,19 +2,64 @@ class ActivitiesController < ApplicationController
   before_action :set_user, only: [:index, :following, :followers]
 
   def index
-
+    set_graph([@user])
   end
 
   def following
-
+    @users = @user.follows
+    set_graph(@users)
   end
 
   def followers
-
+    @users = @user.followers
+    set_graph(@users)
   end
+
+  private
 
   def set_user
     @user = User.find_by(name: params[:user_name])
     @tweet = Tweet.new
   end
+
+  def set_graph(userlist)
+    data_table = []
+
+    userlist.each_with_index do |user,index|
+      a_table = []
+      for i in 0..30
+        date_range = (30-i).day.ago.all_day
+        a_data = []
+        a_data[0] = Date.today.advance(days: -30+i).to_datetime
+        a_data[1] = user.tweets.where(created_at: date_range).count
+        a_table << a_data
+      end
+      data_table << a_table
+    end
+
+    @graph_data = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: "ここ1ヶ月のツイート数")
+      f.chart(zoomType: "x")
+      f.global(useUTC: false)
+      f.xAxis(type: "datetime")
+      f.yAxis(title: {text: "ツイート数"}, allowDecimals: false, min: 0)
+      f.plotOptions(area: {
+
+              marker: {
+                  radius: 2
+              },
+              lineWidth: 2,
+              states: {
+                  hover: {
+                      lineWidth: 3
+                  }
+              },
+              threshold: "null"
+          })
+    userlist.each_with_index do |user,index|
+        f.series(name: "#{user.nickname}のツイート数", type: "area", data: data_table[index])
+      end
+    end
+  end
+
 end
