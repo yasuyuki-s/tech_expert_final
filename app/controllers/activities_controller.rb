f4630b18f6e0c9cf04242d0d8ceee8db3742162e
菅原康之 <yasu@yasu-Mac.local>
2018-01-27 23:09:38 +0900

    曜日別のグラフを追加

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -23,27 +23,34 @@ class ActivitiesController < ApplicationController
   end
 
   def set_graph(userlist)
-    data_table = []
+    data_table_day = []
+    data_table_date = []
 
     userlist.each_with_index do |user,index|
-      a_table = []
-      for i in 0..30
-        date_range = (30-i).day.ago.all_day
-        a_data = []
-        a_data[0] = Date.today.advance(days: -30+i).to_datetime
-        a_data[1] = user.tweets.where(created_at: date_range).count
-        a_table << a_data
+      a_table_day = []
+      a_table_date = []
+      for i in 0..6
+        a_table_day[i] = 0
       end
-      data_table << a_table
+      for i in 0..30
+        data_range = (30-i).day.ago.all_day
+        a_data_date = []
+        a_data_date[0] = Date.today.advance(days: -30+i).to_datetime
+        a_data_date[1] = user.tweets.where(created_at: data_range).count
+        a_table_date << a_data_date
+        a_table_day[(30-i).day.ago.wday] += a_data_date[1]
+      end
+      data_table_date << a_table_date
+      data_table_day << a_table_day
     end
 
-    @graph_data = LazyHighCharts::HighChart.new('graph') do |f|
+    @graph_data_date = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: "ここ1ヶ月のツイート数")
       f.chart(zoomType: "x")
       f.global(useUTC: false)
       f.xAxis(type: "datetime")
       f.yAxis(title: {text: "ツイート数"}, allowDecimals: false, min: 0)
-      f.plotOptions(area: {
+      f.plotOptions(spline: {
 
               marker: {
                   radius: 2
@@ -56,10 +63,38 @@ class ActivitiesController < ApplicationController
               },
               threshold: "null"
           })
-    userlist.each_with_index do |user,index|
-        f.series(name: "#{user.nickname}のツイート数", type: "area", data: data_table[index])
+      userlist.each_with_index do |user,index|
+        f.series(name: "#{user.nickname}のツイート数", type: "spline", data: data_table_date[index])
       end
     end
+
+    @graph_data_day = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: "曜日別")
+      f.chart(type: "spline")
+      f.global(useUTC: false)
+      f.xAxis(categories: [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+        ],plotBands: [{
+            from: -0.5,
+            to: 0.5,
+            color: 'rgba(68, 170, 213, .2)'
+        },{
+            from: 5.5,
+            to: 6.5,
+            color: 'rgba(68, 170, 213, .2)'
+        }])
+      f.yAxis(title: {text: "ツイート数"}, allowDecimals: false, min: 0)
+      userlist.each_with_index do |user,index|
+        f.series(name: "#{user.nickname}のツイート数", data: data_table_day[index])
+      end
+    end
+
   end
 
 end
